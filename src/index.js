4ae68eef06b50a4afc648fb3eee99eabbd6e50a0
acidicMercury8 <acidicmercury8@outlook.com>
2024-10-15 21:47:57 +0300

    Add action sources

---
+++ src/index.js
@@ -0,0 +1,148 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as io from "@actions/io";
+import * as os from "os";
+import * as fs from "fs";
+import * as request from "request";
+import * as compressing from "compressing";
+import * as path from "path";
+
+function getBranch(input)
+{
+    switch (input)
+    {
+        case "1.6-stcop":
+            return "1.6";
+        default:
+            throw new Error(`Not supported branch!`);
+    }
+}
+
+function getArchitecture()
+{
+    const architecture = os.arch();
+    switch (architecture)
+    {
+        case "ia32":
+            return "x86";
+        case "x64":
+            return "x64";
+        default:
+            throw new Error(`Not supported architecture!`);
+    }
+}
+
+async function downloadAsBuffer(url)
+{
+    return new Promise((resolve, reject) =>
+    {
+        core.info(`Downloading file ${url}`);
+        request.get({ url, encoding: null }, (error, responce, body) =>
+        {
+            if (error)
+            {
+                reject(error);
+                return;
+            }
+
+            if (responce.statusCode >= 400)
+            {
+                reject(new Error(`Recieved ${responce.statusCode} from ${url}`));
+            }
+            else
+            {
+                core.info(`Download complete`);
+                resolve(body);
+            }
+        });
+    });
+}
+
+function moveFiles(sourceDirectory, destionationDirectory)
+{
+    return new Promise((resolve, reject) =>
+    {
+        fs.readdir(sourceDirectory, (error, files) =>
+        {
+            if (error)
+            {
+                reject(error);
+                return;
+            }
+
+            files.forEach((file) =>
+            {
+                const oldPath = path.join(sourceDirectory, file);
+                const newPath = path.join(destionationDirectory, file);
+
+                fs.rename(oldPath, newPath, (error) =>
+                {
+                    if (error)
+                    {
+                        reject(error);
+                        return;
+                    }
+
+                    if (files.indexOf(file) === files.length - 1)
+                    {
+                        resolve();
+                    }
+                });
+            });
+        });
+    });
+}
+
+async function run()
+{
+    const platform = os.platform();
+    if (platform !== "win32")
+    {
+        throw new Error(`Not supported platform!`);
+    }
+
+    try
+    {
+        const codebase = core.getInput("codebase");
+        core.debug(`codebase: ${codebase}`);
+
+        const release = core.getInput("release");
+        core.debug(`release: ${release}`);
+
+        const branch = getBranch(codebase);
+        const architecture = getArchitecture().toString();
+        const url =
+            version === "latest"
+                ? `https://github.com/ixray-team/ixray-${codebase}/releases/latest/download/ixray-${branch}-r${release}-utilities-${architecture}-release-bin.zip`
+                : `https://github.com/ixray-team/ixray-${codebase}/releases/download/r${release}/ixray-${branch}-r${release}-utilities-${architecture}-release-bin.zip`;
+        core.debug(`branch: ${branch}`);
+        core.debug(`architecture: ${architecture}`);
+        core.debug(`url: ${url}`);
+
+        const destionation = "bin";
+        await io.mkdirP(destionation);
+
+        const temp = "temp";
+        await io.mkdirP(temp);
+
+        const buffer = await downloadAsBuffer(url);
+        await compressing.zip.uncompress(buffer, temp);
+
+        await moveFiles(path.join(temp, "bin", "Release"), destionation);
+        io.rmRF(temp);
+
+        const filename = "xrCompress.exe";
+        const filepath = path.join(destionation, filename);
+        fs.chmodSync(filepath, "755");
+        core.info(`Successfully installed IX-Ray Compressor ${release}`);
+
+        core.addPath(destionation);
+        core.info(`Successfully added IX-Ray Compressor to PATH`);
+    }
+    catch (error)
+    {
+        core.setFailed(error.message);
+    }
+}
+
+run();
